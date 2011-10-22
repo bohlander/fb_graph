a1a30a6956dec1247e9e24e524ab01849a4337cf
Mike Bohlander <bohlander@gmail.com>
2011-10-21 17:40:59 -0700

    move fql over to graph api

--- lib/fb_graph/query.rb
+++ lib/fb_graph/query.rb
@@ -1,17 +1,16 @@
 module FbGraph
   class Query < Node
-    ENDPOINT = 'https://api.facebook.com/method/fql.query'
-
     attr_accessor :access_token, :query
 
     def initialize(query, access_token = nil)
+      @endpoint = File.join(ROOT_URL, "fql")
       @query = query
       @access_token = access_token
     end
 
     def fetch(access_token = nil)
       handle_response do
-        http_client.get ENDPOINT, :query => build_params(access_token)
+        http_client.get @endpoint, build_params(access_token)
       end
     end
 
@@ -19,26 +18,9 @@ module FbGraph
 
     def build_params(access_token)
       super(
-        :query => self.query,
-        :access_token => access_token || self.access_token,
-        :format => :json
+        :q => self.query,
+        :access_token => access_token || self.access_token
       )
-    end
-
-    def handle_response
-      response = super do
-        yield
-      end
-      case response
-      when Hash
-        if response[:error_code]
-          raise Exception.new(response[:error_code], response[:error_msg])
-        else
-          response
-        end
-      else
-        response
-      end
     end
 
   end
